47147d6b5c6ad519866600537d0461998973d1b0
Stoian Sebastian <stoyan_seby@yahoo.com>
2024-05-28 17:05:41 +0300

    update

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// toggler navbar meniu
-document.querySelector('.navbar-toggler').addEventListener('click', function() {
-  var navbarCollapse = document.querySelector('.navbar-collapse');
-  if (navbarCollapse.classList.contains('show')) {
-    navbarCollapse.classList.remove('show');
-  } else {
-    navbarCollapse.classList.add('show');
-  }
-});
-
-
-// navigatie produse meniu
 const openNav = () => {
-  document.getElementById("myNav").style.width = "33%";
+  document.getElementById("myNav").style.width = "50%";
 };
 
+// Function to close the overlay
 const closeNav = () => {
   document.getElementById("myNav").style.width = "0%";
 };
 
+// Event listeners for opening and closing the overlay
 document.getElementById("openNavBtn").addEventListener("click", openNav);
 document.getElementById("closeNavBtn").addEventListener("click", closeNav);
-document.querySelectorAll(".nav-link").forEach(link => {
+document.querySelectorAll(".overlay-content a").forEach(link => {
   link.addEventListener("click", closeNav);
 });
+
+// Close the overlay if the user clicks anywhere outside of it
+window.onclick = function(event) {
+  const overlay = document.getElementById("myNav");
+  if (event.target == overlay) {
+      closeNav();
+  }
+};
